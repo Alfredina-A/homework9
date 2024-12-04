8335a9d608ca864d44628be4ad4a5f0c6cf0e689
Alfredina-A <daldinart@gmail.com>
2024-12-04 14:22:13 +0000

    adding all files

---
+++ src/index.js
@@ -0,0 +1,31 @@
+let h = document.querySelector(`h1`)
+h.style.textAlign="center"
+h.style.backgroundColor = "blue"
+h.style.color = "white"
+
+
+let dom = document.querySelector(`.dom`)
+dom.style.color = "black"
+dom.style.backgroundColor = "gray"
+dom.innerHTML="I know now what the DOM is 🙌🏽"
+
+let select = document.querySelector(`.select`)
+select.style.color = "black"
+select.style.backgroundColor = "yellow"
+select.innerHTML="I know how to access / select elements in the DOM  👊🏽 "
+
+let stylemethodes = document.querySelector(`.stylemethodes`)
+stylemethodes.style.color = "black"
+stylemethodes.style.backgroundColor = "green"
+stylemethodes.innerHTML="I know how to style methodes to change the style of an element in the DOM ✅ "
+
+let jscss = document.querySelector(`.jscss`)
+// jscss.style.color = "black"
+jscss.style.backgroundColor = "purple"
+jscss.innerHTML="I know how the styling properties differ in JS from CSS 👍🏽 "
+
+let paragraphs = document.querySelector(`.paragraphs`)
+paragraphs.style.color = "white"
+paragraphs.style.backgroundColor = "black"
+paragraphs.innerHTML="Yes I did it!! I manipulated the DOM and styled my elements 😍😍😍😍"
+
